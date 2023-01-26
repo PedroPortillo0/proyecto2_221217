fb398fee27ae4efcd7994dd4c3bcbe86aea9f121
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2023-01-26 09:47:12 -0600

    [UPDATE] actualizaciones formLogin

--- src/components/atoms/FormLogin.jsx
+++ src/components/atoms/FormLogin.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/react.svg";
 
 function FormLogin() {
+  const [descripcion, setDescription]=useState({username:"", password:""})
+  //let descripcion ="soy un componente label"
   const[username,setUsername]=useState('')
 
   const handlerFocus = ()=>{
@@ -11,24 +13,31 @@ function FormLogin() {
   }
 
   const handlerChange =(e) =>{
-   setUsername(e.target.value)
+   setDescription({username: e.target.value, password:descripcion.password})
   }
 
   const handlerClick =(e)=>{
-    e.preventDefault();ñ
+    e.preventDefault();
     console.log(username)
+    setDescription({msn:"hola", v:1, alias:"by"})
   }
+//los estados son asincronos
+//los estados son inmutables 
 
   return (
     <div>
       <img src={Logo} alt="Logo de la empresa" />
       <form>
-        <input type="text"  onFocus={ handlerFocus} onChange= {handlerChange} value={username}  />
-        <input type="password" />
+        <input type="text" value={descripcion.username} onFocus={ handlerFocus} onChange= {handlerChange} />
+        <input type="password" value={descripcion.password}/>
         <button onClick={handlerClick}>Iniciar sesión</button>
+        <div>
+        <label >{descripcion.msn}</label>
+      </div>
         <Link to="/register">Regístrate</Link>
       </form>
     </div>
+    
   );
 }
 
